f5d2a856956f320d841e4d890c2c2f95b8a32629
Rafael <Rafael@TI-01.ogmorj.local>
2015-02-12 19:54:41 -0200

    Criado as variáveis e os getters e setters

--- JAVA III - Ciclo 4 - AV 1/src/br/edu/infnet/java3/ciclo4/av1/entidades/Motocicleta.java
+++ JAVA III - Ciclo 4 - AV 1/src/br/edu/infnet/java3/ciclo4/av1/entidades/Motocicleta.java
@@ -1,5 +1,61 @@
 package br.edu.infnet.java3.ciclo4.av1.entidades;
 
 public class Motocicleta {
-
+	private String chassi;
+	private String montadora;
+	private String modelo;
+	private String tipo;
+	private String cor;
+	private int cilindrada;
+	private int capacidadeTanque;
+	private float preco;
+	
+	public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public String getMontadora() {
+		return montadora;
+	}
+	public void setMontadora(String montadora) {
+		this.montadora = montadora;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public int getCilindrada() {
+		return cilindrada;
+	}
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
 }
